47263f8be673a97ac408fdb635dd6f087a47d3b1
Julio <julesnoir@hotmail.fr>
2022-01-10 11:40:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1> LIBRARY MANAGER </h1>
+<p>Un logiciel de gestion de biliothèque realisé comme projet d'évaluation back end</p>
---
+++ bdd.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS biblioDB;
+
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Book;
+DROP TABLE IF EXISTS Client;
+
+CREATE TABLE User(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    userName varchar(50) UNIQUE NOT NULL,
+    password binary(64) NOT NULL,
+    is_admin boolean NOT NULL
+);
+
+
+CREATE TABLE Client(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    birth_date Date NOT NULL,
+    adress varchar(95) NOT NULL,
+    city varchar(35) NOT NULL,
+    mail varchar(62) UNIQUE NOT NULL,
+    phone varchar(15) UNIQUE NOT NULL,
+    has_books boolean NOT NULL
+);
+
+
+CREATE TABLE Book(
+    id int UNIQUE AUTO_INCREMENT NOT NULL,
+    title varchar(100) NOT NULL,
+    author varchar(100) NOT NULL,
+    summary varchar(400) NOT NULL,
+    release_date Date NOT NULL,
+    category varchar(50) NOT NULL,
+    for_child boolean NOT NULL,
+    available boolean,
+    client_id int NULL,
+    CONSTRAINT FK_client_id FOREIGN KEY (client_id) REFERENCES Client(id)
+);
